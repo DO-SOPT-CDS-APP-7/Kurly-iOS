d9e0bbb5319c4f6870fadf44160914ea4d3e5949
frohsch <kminjoo1213@gmail.com>
2023-11-16 11:34:18 +0900

    [Add] Add project files

---
+++ Kurly/Kurly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kurly
+//
+//  Created by Minjoo Kim on 11/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
