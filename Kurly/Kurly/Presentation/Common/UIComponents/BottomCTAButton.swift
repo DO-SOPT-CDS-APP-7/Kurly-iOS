00aa4fe9b7ce2668dac6c2998faa7ea0e83d5018
frohsch <kminjoo1213@gmail.com>
2023-11-20 01:43:48 +0900

    [Feat] #7 - Add Bottom Button

---
+++ Kurly/Kurly/Presentation/Common/UIComponents/BottomCTAButton.swift
@@ -0,0 +1,81 @@
+//
+//  BottomCTAButton.swift
+//  Kurly
+//
+//  Created by Minjoo Kim on 11/20/23.
+//
+
+import UIKit
+
+import Then
+
+enum BottomCTAButtonType {
+    
+    case buy
+    case more
+    case addCart
+    case order
+    case orderDetail
+    case shopping
+    
+    var title: String {
+        switch self {
+        case .buy:
+            return "구매하기"
+        case .more:
+            return "연관상품 더보기"
+        case .addCart:
+            return "원 장바구니 담기"
+        case .order:
+            return "원 주문하기"
+        case .orderDetail:
+            return "주문 상세보기"
+        case .shopping:
+            return "쇼핑 계속하기"
+        }
+    }
+}
+
+final class BottomCTAButton: UIButton {
+    
+    var titleType: BottomCTAButtonType
+    
+    init(type: BottomCTAButtonType) {
+        self.titleType = type
+        super.init(frame: .zero)
+        setUI()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension BottomCTAButton {
+    
+    private func setUI() {
+        self.do {
+            $0.frame(forAlignmentRect: CGRect(origin: .zero, size: CGSize(width: SizeLiterals.Screen.screenWidth - 32,
+                                                                          height: SizeLiterals.Screen.screenHeight * 50 / 812)))
+            $0.setTitle(titleType.title, for: .normal)
+            $0.titleLabel?.font = .fontGuide(.title_semibold_16)
+            $0.layer.cornerRadius = 6
+            $0.backgroundColor = .kuPurple
+            $0.setTitleColor(.white, for: .normal)
+        }
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints {
+            if(titleType == .buy) {
+                $0.width.equalTo(SizeLiterals.Screen.screenWidth - 87)
+            }
+            else {
+                $0.width.equalTo(SizeLiterals.Screen.screenWidth - 26)
+            }
+            $0.height.equalTo(SizeLiterals.Screen.screenHeight * 50 / 812)
+        }
+    }
+}
