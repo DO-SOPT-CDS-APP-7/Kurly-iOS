50cea869e7f081093ab255dafc6312b563cbbaaf
frohsch <kminjoo1213@gmail.com>
2023-11-20 00:59:37 +0900

    [Feat] #5 - Detail View 기본 세팅

--- Kurly/Kurly/Presentation/Detail/DetailViewController.swift
+++ Kurly/Kurly/Presentation/Detail/DetailViewController.swift
@@ -5,4 +5,36 @@
 //  Created by Minjoo Kim on 11/19/23.
 //
 
-import Foundation
+import UIKit
+
+
+import SnapKit
+import Then
+
+final class DetailViewController: UIViewController {
+    private let navigationBar = CustomNavigationBar(type: .backCartButton)
+    private let tabBarView = TabBarView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        view.addSubviews(navigationBar, tabBarView)
+        
+        navigationBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        tabBarView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(100)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(45)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+    }
+}
