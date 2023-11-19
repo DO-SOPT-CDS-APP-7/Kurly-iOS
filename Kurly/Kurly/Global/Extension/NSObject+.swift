60fbba2eeef899a569a143826d4025eea4d50fdb
frohsch <kminjoo1213@gmail.com>
2023-11-19 19:08:25 +0900

    [Setting] #3 - Add BaseView, BaseViewController

--- Kurly/Kurly/Global/Extension/NSObject+.swift
+++ Kurly/Kurly/Global/Extension/NSObject+.swift
@@ -9,6 +9,10 @@ import Foundation
 
 extension NSObject {
 
+    var className: String {
+        NSStringFromClass(self.classForCoder).components(separatedBy: ".").last!
+    }
+    
     static var className: String {
         NSStringFromClass(self.classForCoder()).components(separatedBy: ".").last!
     }
