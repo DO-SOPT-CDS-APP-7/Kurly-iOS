44b8aa57fc159b788d8b4e8cb3c9f7f47ee7447d
boyeon <judy010119@gmail.com>
2023-11-21 01:36:04 +0900

    [Feat] #15 - Add DetailProduct Model

--- Kurly/Kurly/Presentation/Detail/Models/DetailProduct.swift
+++ Kurly/Kurly/Presentation/Detail/Models/DetailProduct.swift
@@ -5,4 +5,29 @@
 //  Created by 김보연 on 11/20/23.
 //
 
-import Foundation
+import UIKit
+
+struct DetailProduct {
+    let id: Int
+    let image: UIImage
+    let delivery: String
+    let name: String
+    let description: String
+    let salePercent: String
+    let salePrice: Int
+    let price: Int
+}
+
+extension DetailProduct {
+    static func dummy() -> DetailProduct {
+        return DetailProduct(id: 0,
+                             image: ImageLiterals.Home.img.large,
+                             delivery: "샛별배송",
+                             name: "[전주 베테랑] 칼국수",
+                             description: "베테랑의 대표메뉴를 집에서",
+                             salePercent: "20%",
+                             salePrice: 4720,
+                             price: 5900)
+    }
+}
+
