2d9071525976f5d788bcc52e6a0731758df2b6c0
frohsch <kminjoo1213@gmail.com>
2023-11-20 13:49:22 +0900

    [Feat] #9 - Add Product Model

--- Kurly/Kurly/Presentation/AddCart/Models/Product.swift
+++ Kurly/Kurly/Presentation/AddCart/Models/Product.swift
@@ -5,4 +5,25 @@
 //  Created by Minjoo Kim on 11/20/23.
 //
 
-import Foundation
+import UIKit
+
+struct Product {
+    let id: Int
+    let image: UIImage
+    let name: String
+    let description: String
+    let salePrice: Int
+    let price: Int
+}
+
+extension Product {
+    static func dummy() -> Product {
+        return Product(id: 0,
+                       image: ImageLiterals.addCart.smallCart,
+                       name: "[전주 베테랑] 칼국수",
+                       description: "베테랑의 대표메뉴를 집에서",
+                       salePrice: 4720,
+                       price: 5900)
+    }
+}
+
