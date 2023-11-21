04e6da9d0942f594f193ba22a51d03ddd98ca0c6
Minsoo Kang <alstn38@naver.com>
2023-11-21 16:04:52 +0900

    [Feat] #10 - 찜하기 버튼 취소시 나오는 ViewController 구현 추후 DetailViewController에서 작동할 예정

--- Kurly/Kurly/Presentation/FavoriteFood/ViewControllers/sampleViewController.swift
+++ Kurly/Kurly/Presentation/FavoriteFood/ViewControllers/sampleViewController.swift
@@ -2,7 +2,71 @@
 //  sampleViewController.swift
 //  Kurly
 //
-//  Created by 강민수 on 11/21/23.
+//  Created by 강민수 on 11/20/23.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+class sampleViewController: BaseViewController {
+    
+    private let favoriteButton = UIButton()
+    private let notifyAddToastView = NotifyAddToastView()
+    private let notifyRemoveToastView = NotifyRemoveToastView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+        
+        favoriteButton.addTarget(self, action: #selector(favoriteButtonTapped), for: .touchUpInside)
+    }
+    
+    /// View 의 Style 을 set 합니다.
+    override func setUI() {
+        view.backgroundColor = .white
+        
+        favoriteButton.do {
+            $0.backgroundColor = .red
+        }
+    }
+    /// View 의 Layout 을 set 합니다.
+    override func setLayout() {
+        view.addSubviews(favoriteButton, notifyAddToastView, notifyRemoveToastView)
+        
+        favoriteButton.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.size.equalTo(44)
+        }
+        
+        notifyAddToastView.snp.makeConstraints {
+            $0.bottom.equalTo(view.snp.bottom)
+            $0.height.equalTo(51)
+            $0.leading.trailing.equalToSuperview().inset(8)
+        }
+        
+        notifyRemoveToastView.snp.makeConstraints {
+            $0.bottom.equalTo(view.snp.bottom)
+            $0.height.equalTo(51)
+            $0.leading.trailing.equalToSuperview().inset(8)
+        }
+    }
+    
+    @objc private func favoriteButtonTapped() {
+        //찜 삭제했을 때
+        notifyRemoveToastView.viewToastView()
+        
+        //모달 생성
+//        let relatedFoodModalViewController = RelatedFoodModalViewController()
+//        relatedFoodModalViewController.modalPresentationStyle = .automatic
+//        self.present(relatedFoodModalViewController, animated: true)
+    }
+    
+    /// View 의 Delegate 을 set 합니다.
+    override func setDelegates() {}
+    /// View 의 Register 를 set 합니다.
+    override func setRegister() {}
+    
+}
