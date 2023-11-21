af3e7a3b1ef59f6162c396ad82aad7aad3e7d7a5
Minsoo Kang <alstn38@naver.com>
2023-11-21 16:02:09 +0900

    [Feat] #10 - 찜하기 버튼 클릭시 나오는 토스트 뷰 구현

---
+++ Kurly/Kurly/Presentation/FavoriteFood/View/NotifyRemoveToastView.swift
@@ -0,0 +1,8 @@
+//
+//  NotifyRemoveToastView.swift
+//  Kurly
+//
+//  Created by 강민수 on 11/21/23.
+//
+
+import Foundation
---
+++ Kurly/Kurly/Presentation/FavoriteFood/ViewControllers/sampleViewController.swift
@@ -0,0 +1,8 @@
+//
+//  sampleViewController.swift
+//  Kurly
+//
+//  Created by 강민수 on 11/21/23.
+//
+
+import Foundation
