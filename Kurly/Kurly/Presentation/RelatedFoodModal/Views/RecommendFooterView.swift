90f00809ff9fb4b200fb2b329bf5bfcb4bf058ef
Minsoo Kang <alstn38@naver.com>
2023-11-21 22:57:15 +0900

    [Feat] #20 - 연관음식 CollectionViewCell, HeaderView, FooterView 구현

--- Kurly/Kurly/Presentation/RelatedFoodModal/Views/RecommendFooterView.swift
+++ Kurly/Kurly/Presentation/RelatedFoodModal/Views/RecommendFooterView.swift
@@ -5,4 +5,30 @@
 //  Created by 강민수 on 11/21/23.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+class RecommendFooterView: UICollectionReusableView {
+    
+    static let identifier: String = "RecommendFooterView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        self.do {
+            $0.backgroundColor = .gray1
+        }
+    }
+    
+    private func setLayout() {}
+}
--- Kurly/Kurly/Presentation/RelatedFoodModal/Views/RecommendHeaderView.swift
+++ Kurly/Kurly/Presentation/RelatedFoodModal/Views/RecommendHeaderView.swift
@@ -5,4 +5,48 @@
 //  Created by 강민수 on 11/21/23.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+class RecommendHeaderView: UICollectionReusableView {
+    
+    static let identifier: String = "RecommendHeaderView"
+    private let sectionLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        self.do {
+            $0.backgroundColor = .white
+        }
+        
+        sectionLabel.do {
+            $0.text = "다른 고객이 함께 본 상품"
+            $0.font = .fontGuide(.title_semibold_16)
+            $0.textColor = .gray6
+        }
+    }
+    
+    private func setLayout() {
+        addSubview(sectionLabel)
+        
+        sectionLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    func bindData(sectionText: String) {
+        sectionLabel.text = sectionText
+    }
+}
