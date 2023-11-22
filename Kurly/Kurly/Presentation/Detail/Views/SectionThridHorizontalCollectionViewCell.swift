2499589011f1a704dc0a38cf26ac7fe87c90a1a0
boyeon <judy010119@gmail.com>
2023-11-23 01:39:43 +0900

    [Feat] #31 - 세 번째 섹션 수평스크롤 컬렉션 뷰 구현

---
+++ Kurly/Kurly/Presentation/Detail/Views/SectionThridHorizontalCollectionViewCell.swift
@@ -0,0 +1,90 @@
+//
+//  SectionThridHorizontalCollectionViewCell.swift
+//  Kurly
+//
+//  Created by 김보연 on 11/23/23.
+//
+
+import UIKit
+
+class SectionThridHorizontalCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier: String = "SectionThridHorizontalCollectionViewCell"
+    
+    private lazy var horizontalCollectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
+    
+    private let relatedFoodModalView = RelatedFoodModalView()
+    private var relatedFoodList: [RecommendModel] = [
+        .init(foodImage: ImageLiterals.Home.img.activityTop01, foodName: "[시골보쌈과 감자옹심이 감자...", foodPrice: "10,500원"),
+        .init(foodImage: ImageLiterals.Home.img.activityTop02, foodName: "[이연복의 목란] 짬뽕 2인분...", foodPrice: "13,800원"),
+        .init(foodImage: ImageLiterals.Home.img.activityTop03, foodName: "[방방곡곡] 비빔국수 키트(2인...", foodPrice: "9,900원"),
+        .init(foodImage: ImageLiterals.Home.img.activityTop01, foodName: "[시골보쌈과 감자옹심이 감자...", foodPrice: "10,500원"),
+        .init(foodImage: ImageLiterals.Home.img.activityTop02, foodName: "[이연복의 목란] 짬뽕 2인분...", foodPrice: "13,800원"),
+        .init(foodImage: ImageLiterals.Home.img.activityTop03, foodName: "[방방곡곡] 비빔국수 키트(2인...", foodPrice: "9,900원")
+    ]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        basicSetup()
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func basicSetup() {
+        self.horizontalCollectionView.register(RecommendCollectionViewCell.self,
+                                               forCellWithReuseIdentifier: RecommendCollectionViewCell.identifier)
+        relatedFoodModalView.recommendCollectionView.register(RecommendCollectionViewCell.self,
+                                                              forCellWithReuseIdentifier: RecommendCollectionViewCell.identifier)
+        self.horizontalCollectionView.delegate = self
+        self.horizontalCollectionView.dataSource = self
+    }
+    
+    private func setUI() {
+        horizontalCollectionView.do {
+            $0.backgroundColor = .clear
+            let flowLayout = UICollectionViewFlowLayout()
+            flowLayout.scrollDirection = .horizontal
+            $0.collectionViewLayout = flowLayout
+        }
+    }
+    
+    private func setLayout() {
+        self.addSubview(horizontalCollectionView)
+        
+        horizontalCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension SectionThridHorizontalCollectionViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: RecommendCollectionViewCell.identifier, for: indexPath) as? RecommendCollectionViewCell else { return UICollectionViewCell()}
+        item.bindData(cellData: relatedFoodList[indexPath.row])
+        return item
+    }
+}
+
+extension SectionThridHorizontalCollectionViewCell: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 120, height: 276)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout flowLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(8)
+    }
+}
