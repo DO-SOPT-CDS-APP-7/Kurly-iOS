471881b491811d145669d6ba164b51a68e230d2d
frohsch <kminjoo1213@gmail.com>
2023-11-22 23:20:26 +0900

    [Feat] #12 - Modify AfterAddCartView UI

--- Kurly/Kurly/Application/SceneDelegate.swift
+++ Kurly/Kurly/Application/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = DetailViewController()
+        window.rootViewController = TestViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
