96da677f12a03f61556691ce964d22dbf3309d9f
boyeon <judy010119@gmail.com>
2023-11-24 06:53:48 +0900

    [Feat] #47 - 하단 바 생성

--- Kurly/Kurly/Presentation/Detail/Views/DetailBottomBarView.swift
+++ Kurly/Kurly/Presentation/Detail/Views/DetailBottomBarView.swift
@@ -5,4 +5,48 @@
 //  Created by 김보연 on 11/24/23.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class DetailBottomBarView: UIView {
+    
+    let bottomDibsButton = BottomDibsButton()
+    let bottomCTAButton = BottomCTAButton(type: .buy)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension DetailBottomBarView {
+    
+    private func setUI() {
+        self.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        self.addSubviews(bottomDibsButton, bottomCTAButton)
+        
+        self.snp.makeConstraints {
+            $0.height.equalTo(SizeLiterals.Screen.screenHeight * 108 / 812)
+        }
+        
+        bottomDibsButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(13)
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).offset(-11)
+        }
+        
+        bottomCTAButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(73)
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).offset(-11)
+        }
+    }
+}
