1a886f35e6c29086505d68f0b3ddf8a62f93db8f
Genesis <dmsgktn1685@naver.com>
2023-11-27 02:28:21 +0900

    [Feat] #61 - CartView 에 분기처리하였습니다

--- Kurly/Kurly/Presentation/Cart/Views/CartView.swift
+++ Kurly/Kurly/Presentation/Cart/Views/CartView.swift
@@ -10,16 +10,23 @@ import UIKit
 import SnapKit
 import Then
 
-class CartView: BaseView {
+final class CartView: BaseView {
     
+    var cartType: CartViewType
+    let cartHeaderView: CartHeaderView
+    let bottomCTAButton: BottomCTAButton
     let navigationBar = CustomNavigationBar(type: .closeButton)
-    let bottomCTAButton = BottomCTAButton(type: .order)
-    let cartHeaderView = CartHeaderView()
     let cartItemCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     private let divider = UIView()
+    private let emptyItemView = EmptyItemView()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    init(type: CartViewType) {
+        self.cartType = type
+        self.cartHeaderView = CartHeaderView(type: type)
+        self.bottomCTAButton = BottomCTAButton(type: type == .emptyCart ? .emptyCart : .order)
+        super.init(frame: .zero)
+        setUI()
+        setLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -28,20 +35,22 @@ class CartView: BaseView {
     
     override func setUI() {
         self.backgroundColor = .white
-        
-        cartItemCollectionView.do {
-            $0.backgroundColor = .white
-            $0.showsVerticalScrollIndicator = false
-        }
-        
+
         divider.do {
             $0.backgroundColor = . gray2
+        }
+        
+        if cartType == .order {
+            cartItemCollectionView.do {
+                $0.backgroundColor = .white
+                $0.showsVerticalScrollIndicator = false
+            }
         }
     }
     
     override func setLayout() {
-        self.addSubviews(navigationBar, cartHeaderView, cartItemCollectionView, bottomCTAButton, divider)
-
+        self.addSubviews(navigationBar, cartHeaderView, divider, bottomCTAButton)
+        
         navigationBar.snp.makeConstraints {
             $0.top.equalToSuperview().offset(48)
             $0.horizontalEdges.equalToSuperview()
@@ -57,16 +66,28 @@ class CartView: BaseView {
             $0.horizontalEdges.equalToSuperview()
             $0.height.equalTo(8)
         }
-        
-        cartItemCollectionView.snp.makeConstraints {
-            $0.top.equalTo(divider.snp.bottom)
-            $0.horizontalEdges.equalToSuperview()
-            $0.bottom.equalTo(bottomCTAButton.snp.top).inset(-9)
-        }
-        
+
         bottomCTAButton.snp.makeConstraints {
             $0.horizontalEdges.equalToSuperview().inset(16)
             $0.bottom.equalTo(self.safeAreaLayoutGuide).inset(16)
+        }
+        
+        if cartType == .emptyCart {
+            self.addSubview(emptyItemView)
+            
+            emptyItemView.snp.makeConstraints {
+                $0.top.equalTo(divider.snp.top).offset(233)
+                $0.horizontalEdges.equalToSuperview()
+                $0.bottom.equalTo(bottomCTAButton.snp.top).inset(-214)
+            }
+        } else if cartType == .order {
+            self.addSubview(cartItemCollectionView)
+            
+            cartItemCollectionView.snp.makeConstraints {
+                $0.top.equalTo(divider.snp.bottom)
+                $0.horizontalEdges.equalToSuperview()
+                $0.bottom.equalTo(bottomCTAButton.snp.top).inset(-9)
+            }
         }
     }
 }
