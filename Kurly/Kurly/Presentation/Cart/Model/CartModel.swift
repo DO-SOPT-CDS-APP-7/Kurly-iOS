49bf5a8178eb0467f44ac4d720fa2acf93aad415
Genesis <dmsgktn1685@naver.com>
2023-11-27 22:42:17 +0900

    [Feat] #68 - Implement CartModel

--- Kurly/Kurly/Presentation/Cart/Model/CartModel.swift
+++ Kurly/Kurly/Presentation/Cart/Model/CartModel.swift
@@ -13,10 +13,22 @@ struct CartModel {
     var originalPrice: Double
     var discountRate: Double
     let imageURL: String
-    
-    var discountedPrice: Double {
+    var isSelect: Bool = false
+    var discountedPrice: Int {
         let discountAmount = originalPrice * discountRate / 100
-        return originalPrice - discountAmount
+        return Int(originalPrice - discountAmount) * itemCount
+    }
+    
+    var calculatePrice: Int {
+        return Int(originalPrice * Double(itemCount))
+    }
+
+    var itemCount: Int {
+        didSet {
+            if itemCount < 1 {
+                itemCount = 1
+            }
+        }
     }
 }
 
@@ -26,17 +38,17 @@ extension CartModel {
                           productName: "[시골보쌈과 감자옹심이] 감자옹심이 칼국수(2인분)",
                           originalPrice: 10500,
                           discountRate: 50,
-                          imageURL: "https://insopt-seminar3.s3.ap-northeast-2.amazonaws.com/marketKurly/kurly1.png"),
+                          imageURL: "https://insopt-seminar3.s3.ap-northeast-2.amazonaws.com/marketKurly/kurly1.png", itemCount: 1),
                 CartModel(id: 1,
                           productName: "[올면] 속초식 명태 회냉면 2인분",
                           originalPrice: 12000,
                           discountRate: 50,
-                          imageURL: "https://insopt-seminar3.s3.ap-northeast-2.amazonaws.com/marketKurly/kurly3.svg"),
+                          imageURL: "https://insopt-seminar3.s3.ap-northeast-2.amazonaws.com/marketKurly/kurly3.svg", itemCount: 1),
                 CartModel(id: 2,
                           productName: "[광화문 미진] 메밀국수 (2인분)",
                           originalPrice: 9980,
                           discountRate: 50,
-                          imageURL: "https://insopt-seminar3.s3.ap-northeast-2.amazonaws.com/marketKurly/kurly4.svg")]
+                          imageURL: "https://insopt-seminar3.s3.ap-northeast-2.amazonaws.com/marketKurly/kurly4.svg", itemCount: 1)]
     }
 }
 
