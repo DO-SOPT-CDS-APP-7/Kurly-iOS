cd54bce94c66b213eca0f34ad0a66db88ed5708b
Genesis <dmsgktn1685@naver.com>
2023-11-27 22:41:55 +0900

    [Feat] #68 - Implement OrderModel

---
+++ Kurly/Kurly/Presentation/Cart/Model/OrderModel.swift
@@ -0,0 +1,15 @@
+//
+//  OrderModel.swift
+//  Kurly
+//
+//  Created by ParkJunHyuk on 2023/11/27.
+//
+
+import Foundation
+
+struct OrderModel {
+    let itemPrice: Int
+    let discountedPrice: Int
+    let deliveryPrice: Int
+    let totalPrice: Int
+}
