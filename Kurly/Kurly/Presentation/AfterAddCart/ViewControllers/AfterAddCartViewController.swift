e9df42ea069a4fe3ec127a6e89343c2591c6dc40
frohsch <kminjoo1213@gmail.com>
2023-11-27 18:16:53 +0900

    [Chore] #64 - Delete Comments

--- Kurly/Kurly/Presentation/AfterAddCart/ViewControllers/AfterAddCartViewController.swift
+++ Kurly/Kurly/Presentation/AfterAddCart/ViewControllers/AfterAddCartViewController.swift
@@ -43,31 +43,3 @@ extension AfterAddCartViewController {
         print("tapButton")
     }
 }
-//
-//extension AfterAddCartViewController: UICollectionViewDataSource {
-//    
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return dummy.count
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RelatedCollectionViewCell.identifier, for: indexPath) as? RelatedCollectionViewCell else { return UICollectionViewCell()}
-//        cell.bindData(model: dummy[indexPath.row])
-//        return cell
-//    }
-//}
-//
-//extension AfterAddCartViewController: UICollectionViewDelegateFlowLayout {
-//    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize(width: (SizeLiterals.Screen.screenWidth - 52) / 3 , height: 257)
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-//        return UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, layout flowLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        return 8.0
-//    }
-//}
