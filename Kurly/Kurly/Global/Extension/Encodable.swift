bf1c89b80033e86c16d98e8371ebb9256322775b
frohsch <kminjoo1213@gmail.com>
2023-11-28 10:07:17 +0900

    [Setting] #74 - Add Encodable Extension

--- Kurly/Kurly/Global/Extension/Encodable.swift
+++ Kurly/Kurly/Global/Extension/Encodable.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension Encodable {
+    func toDictionary() -> [String: Any] {
+        guard let data = try? JSONEncoder().encode(self),
+              let jsonData = try? JSONSerialization.jsonObject(with: data),
+              let dictionaryData = jsonData as? [String: Any] else { return [:] }
+        return dictionaryData
+    }
+}
