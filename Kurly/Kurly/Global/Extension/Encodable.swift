7ebb6d3f62288600b1f6aa7315024c09cffd47b9
frohsch <kminjoo1213@gmail.com>
2023-11-28 10:06:33 +0900

    [Setting] #73 - Add Gitignore

---
+++ Kurly/Kurly/Global/Extension/Encodable.swift
@@ -0,0 +1,8 @@
+//
+//  Encodable.swift
+//  Kurly
+//
+//  Created by Minjoo Kim on 11/28/23.
+//
+
+import Foundation
---
+++ Kurly/Kurly/Network/Service/ProductService.swift
@@ -0,0 +1,8 @@
+//
+//  ProductService.swift
+//  Kurly
+//
+//  Created by Minjoo Kim on 11/28/23.
+//
+
+import Foundation
