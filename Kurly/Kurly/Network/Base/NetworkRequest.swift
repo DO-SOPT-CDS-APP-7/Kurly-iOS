c75cb4903a0350c86b83ef0c1c6f7f7954697ab8
frohsch <kminjoo1213@gmail.com>
2023-11-28 10:10:17 +0900

    [Setting] #73 - Add NetworkRequest

--- Kurly/Kurly/Network/Base/NetworkRequest.swift
+++ Kurly/Kurly/Network/Base/NetworkRequest.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+struct NetworkRequest {
+    let path: String
+    let httpMethod: HttpMethod
+    let query: Request?
+    let body: Data?
+    let header: [String: String]?
+
+    init(path: String, httpMethod: HttpMethod, query: Request? = nil, body: Data? = nil, header: [String : String]? = nil) {
+        self.path = path
+        self.httpMethod = httpMethod
+        self.query = query
+        self.body = body
+        self.header = header
+    }
+
+    func makeURLRequest() throws -> URLRequest {
+        var urlComponents = URLComponents(string: Config.baseURL)
+
+        if let query = self.query {
+            let queries = query.toDictionary()
+            let queryItemArray = queries.map {
+                return URLQueryItem(name: $0.key, value: "\($0.value)")
+            }
+            urlComponents?.queryItems = queryItemArray
+        }
+
+        guard let urlRequestURL = urlComponents?.url?.appendingPathComponent(self.path) else {
+            throw NetworkError.urlEncodingError
+        }
+
+        var urlRequest = URLRequest(url: urlRequestURL)
+        urlRequest.httpMethod = self.httpMethod.rawValue
+        urlRequest.setValue(ContentType.json.rawValue, forHTTPHeaderField: HTTPHeaderField.contentType.rawValue)
+        urlRequest.httpBody = self.body
+        print(urlRequest)
+        return urlRequest
+    }
+
+}
