99d2e645d9d6653546f3f865331bfa333e4ea2dd
Genesis <dmsgktn1685@naver.com>
2023-11-28 20:13:57 +0900

    [Feat] #82 - change Scroll yOffset & Autolayout

--- Kurly/Kurly/Presentation/Cart/Views/CartView.swift
+++ Kurly/Kurly/Presentation/Cart/Views/CartView.swift
@@ -20,6 +20,9 @@ final class CartView: BaseView {
     private let divider = UIView()
     private let emptyItemView = EmptyItemView()
     
+    private var cartAddressViewHeight: CGFloat?
+    private var navigationBarViewHeight: CGFloat?
+    
     init(type: CartViewType) {
         self.cartType = type
         self.cartHeaderView = CartHeaderView(type: type)
@@ -27,15 +30,22 @@ final class CartView: BaseView {
         super.init(frame: .zero)
         setUI()
         setLayout()
+        layoutSubviews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        cartAddressViewHeight = cartHeaderView.cartAddressView.frame.size.height
+        navigationBarViewHeight = navigationBar.frame.size.height
+    }
+    
     override func setUI() {
         self.backgroundColor = .white
-
+        
         divider.do {
             $0.backgroundColor = . gray2
         }
@@ -44,15 +54,26 @@ final class CartView: BaseView {
             cartItemCollectionView.do {
                 $0.backgroundColor = .white
                 $0.showsVerticalScrollIndicator = false
+                $0.contentInset = UIEdgeInsets(top: 167 + 8, left: 0, bottom: 0, right: 0)
             }
         }
     }
     
     override func setLayout() {
-        self.addSubviews(navigationBar, cartHeaderView, divider, bottomCTAButton)
         
+        if cartType == .order {
+            self.addSubview(cartItemCollectionView)
+        
+            cartItemCollectionView.snp.makeConstraints {
+                $0.top.equalTo(self.safeAreaLayoutGuide)
+                $0.horizontalEdges.equalToSuperview()
+                $0.bottom.equalTo(self.safeAreaLayoutGuide).inset(80)
+            }
+        }
+        
+        self.addSubviews(navigationBar, cartHeaderView, divider, bottomCTAButton)
         navigationBar.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(48)
+            $0.top.equalTo(self.safeAreaLayoutGuide)
             $0.horizontalEdges.equalToSuperview()
         }
         
@@ -80,14 +101,6 @@ final class CartView: BaseView {
                 $0.horizontalEdges.equalToSuperview()
                 $0.bottom.equalTo(bottomCTAButton.snp.top).inset(-214)
             }
-        } else if cartType == .order {
-            self.addSubview(cartItemCollectionView)
-            
-            cartItemCollectionView.snp.makeConstraints {
-                $0.top.equalTo(divider.snp.bottom)
-                $0.horizontalEdges.equalToSuperview()
-                $0.bottom.equalTo(bottomCTAButton.snp.top).inset(-9)
-            }
         }
     }
 }
@@ -96,5 +109,31 @@ extension CartView {
     
     func bindPrice(totalPrice: Int) {
         self.bottomCTAButton.setTitle("\(totalPrice.priceText) 주문하기", for: .normal)
+    }
+}
+
+extension CartView {
+    
+    func updateView(forScrollOffset yOffset: CGFloat) {
+    
+        let scrollyOffset = -yOffset - 175
+        
+        let cartAddressViewAlpha = max((30 + scrollyOffset) / (30 - 0), 0)
+        let dividerAlpha = max((10 + scrollyOffset) / (10 - 0), 0)
+        
+        cartHeaderView.cartAddressView.alpha = cartAddressViewAlpha
+        cartHeaderView.divider.alpha = dividerAlpha
+
+        let allSecletedItemViewTopConstraint = max(72 + scrollyOffset, 0)
+
+        if cartHeaderView.cartAddressView.alpha == 0 {
+            cartHeaderView.cartAddressView.isHidden = true
+            
+            cartHeaderView.headerTopConstraint?.update(offset: allSecletedItemViewTopConstraint)
+        } else {
+            cartHeaderView.cartAddressView.isHidden = false
+
+            cartHeaderView.headerTopConstraint?.update(offset: allSecletedItemViewTopConstraint)
+        }
     }
 }
