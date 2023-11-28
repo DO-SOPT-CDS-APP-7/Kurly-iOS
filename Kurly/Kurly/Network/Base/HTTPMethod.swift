b766dcd8c83fc569cc359f27e4882a63a35a28a9
frohsch <kminjoo1213@gmail.com>
2023-11-28 10:11:07 +0900

    [Setting] #73 - Add HTTPMethod

--- Kurly/Kurly/Network/Base/HTTPMethod.swift
+++ Kurly/Kurly/Network/Base/HTTPMethod.swift
@@ -5,4 +5,22 @@
 //  Created by Minjoo Kim on 11/28/23.
 //
 
-import Foundation
+import UIKit
+
+enum HttpMethod: String {
+    case `get`
+    case post
+    case put
+    case patch
+    case delete
+
+    var rawValue: String {
+        switch self {
+        case .get: return "GET"
+        case .post: return "POST"
+        case .put: return "PUT"
+        case .patch: return "PATCH"
+        case .delete: return "DELETE"
+        }
+    }
+}
