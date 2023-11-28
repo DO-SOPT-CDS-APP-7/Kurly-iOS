4dd547a30e2a842a47456eecb27adf567188cf41
frohsch <kminjoo1213@gmail.com>
2023-11-28 10:09:05 +0900

    [Setting] #73 - Add Network Error

--- Kurly/Kurly/Network/Base/NetworkError.swift
+++ Kurly/Kurly/Network/Base/NetworkError.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+@frozen
+enum NetworkError: Error, CustomStringConvertible {
+    case urlEncodingError
+    case jsonDecodingError
+    case badCasting
+    case fetchImageError
+    case unAuthorizedError
+    case clientError(code: String, message: String)
+    case serverError
+    
+    var description: String {
+        switch self {
+        case .urlEncodingError:
+            return "🔒URL Encoding Error"
+        case .jsonDecodingError:
+            return "🔐JSON Decoding Error"
+        case .badCasting:
+            return "❌Bad Casting (HTTPResponse)"
+        case .fetchImageError:
+            return "🌠FetchImageError"
+        case .unAuthorizedError:
+            return "🚪UnAuthorized Error"
+        case .clientError(let code, let message):
+            return "📱Client Error code: \(code), message:\(message)"
+        case .serverError:
+            return "🖥️Server Error"
+        }
+    }
+}
