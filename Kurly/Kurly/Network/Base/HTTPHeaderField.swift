59c64a2bd4a819586da30bdf08e337166a0306e5
frohsch <kminjoo1213@gmail.com>
2023-11-28 10:10:43 +0900

    [Setting] #73 - Add Network HTTP Header

--- Kurly/Kurly/Network/Base/HTTPHeaderField.swift
+++ Kurly/Kurly/Network/Base/HTTPHeaderField.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+enum HTTPHeaderField: String {
+    case authentication = "Authorization"
+    case contentType = "Content-Type"
+    case acceptType = "Accept"
+}
+
+enum ContentType: String {
+    case json = "Application/json"
+}
