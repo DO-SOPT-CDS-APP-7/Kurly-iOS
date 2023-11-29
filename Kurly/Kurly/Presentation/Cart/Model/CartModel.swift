482c45103cfda5dab47ea1d2b4fb16a08c53adeb
Genesis <dmsgktn1685@naver.com>
2023-11-29 20:39:16 +0900

    [Fix] #91 - delete id

--- Kurly/Kurly/Presentation/Cart/Model/CartModel.swift
+++ Kurly/Kurly/Presentation/Cart/Model/CartModel.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 struct CartModel {
-    let id: Int
     let productName: String
     var originalPrice: Double
     var discountRate: Double
@@ -34,18 +33,15 @@ struct CartModel {
 
 extension CartModel {
     static func dummy() -> [CartModel] {
-        return [CartModel(id: 0,
-                          productName: "[시골보쌈과 감자옹심이] 감자옹심이 칼국수(2인분)",
+        return [CartModel(productName: "[시골보쌈과 감자옹심이] 감자옹심이 칼국수(2인분)",
                           originalPrice: 10500,
                           discountRate: 50,
                           imageURL: "https://insopt-seminar3.s3.ap-northeast-2.amazonaws.com/marketKurly/kurly1.png", itemCount: 1),
-                CartModel(id: 1,
-                          productName: "[올면] 속초식 명태 회냉면 2인분",
+                CartModel(productName: "[올면] 속초식 명태 회냉면 2인분",
                           originalPrice: 12000,
                           discountRate: 50,
                           imageURL: "https://insopt-seminar3.s3.ap-northeast-2.amazonaws.com/marketKurly/kurly3.svg", itemCount: 1),
-                CartModel(id: 2,
-                          productName: "[광화문 미진] 메밀국수 (2인분)",
+                CartModel(productName: "[광화문 미진] 메밀국수 (2인분)",
                           originalPrice: 9980,
                           discountRate: 50,
                           imageURL: "https://insopt-seminar3.s3.ap-northeast-2.amazonaws.com/marketKurly/kurly4.svg", itemCount: 1)]
