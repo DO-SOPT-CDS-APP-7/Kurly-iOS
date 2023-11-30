19f3fa1fcbb7dda488e49ad88e40c939003c7a0f
Genesis <dmsgktn1685@naver.com>
2023-11-30 22:00:13 +0900

    [Feat] #91 - Implement CartCheckService

---
+++ Kurly/Kurly/Network/Service/CartCheckService.swift
@@ -0,0 +1,46 @@
+//
+//  CartCheckService.swift
+//  Kurly
+//
+//  Created by ParkJunHyuk on 2023/11/29.
+//
+
+import Foundation
+
+final class CartCheckService {
+    
+    private let apiService: Requestable
+    
+    init(apiService: Requestable) {
+        self.apiService = apiService
+    }
+    
+    func getCartResponse(xAuthId: Int) async throws -> [CartResponse]? {
+        let urlRequest = try NetworkRequest(path: "/cart", httpMethod: .get, header: ["X-Auth-id": "\(xAuthId)"]).makeURLRequest()
+        
+        return try await apiService.request(urlRequest)
+    }
+    
+    func fetchCart(xAuthId: Int) async throws -> [CartModel] {
+        guard let model = try await self.getCartResponse(xAuthId: xAuthId) else {
+            throw NetworkError.badCasting
+        }
+
+        guard let iamge = try await KingfisherService.fetchImage(with: model.imageURL) else {
+            throw NetworkError.badCasting
+        }
+        
+        return createCartModelData(cartData: model)
+    }
+    
+    func createCartModelData(cartData: [CartResponse]) -> [CartModel] {
+        
+        var cartModelData: [CartModel] = []
+        
+        cartData.forEach { data in
+            cartModelData.append(CartModel(productName: data.productName, originalPrice: Double(data.price), discountRate: Double(data.discountRate), imageURL: data.imageURL, itemCount: data.count))
+        }
+        
+        return cartModelData
+    }
+}
