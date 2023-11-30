5617c8bf7b5d94db3111c8f2573cf8f2d656400a
boyeon <judy010119@gmail.com>
2023-11-30 21:58:06 +0900

    [Feature] #100 - RelatedResponse 파일 생성

---
+++ Kurly/Kurly/Network/DTO/RelatedResponse.swift
@@ -0,0 +1,15 @@
+//
+//  RelatedResponse.swift
+//  Kurly
+//
+//  Created by 김보연 on 11/30/23.
+//
+
+import UIKit
+
+struct RelatedResponse: Response {
+    let deliveryType: String
+    let productName: String
+    let originalPrice: Int
+    let imageURL: String
+}
---
+++ Kurly/Kurly/Network/Service/RelatedProductService.swift
@@ -0,0 +1,8 @@
+//
+//  RelatedProductService.swift
+//  Kurly
+//
+//  Created by 김보연 on 11/30/23.
+//
+
+import Foundation
